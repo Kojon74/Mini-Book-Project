2bd16edb60b524e0df55e5fa5d0cd18830b83963
Ken Johnson <dragonkenkj@gmail.com>
2021-01-12 06:32:21 -0500

    Import/export

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 0,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/41vw+RmEi4L._SX327_BO1,204,203,200_.jpg",
+    title: "The Martian",
+    author: "Andy Weir",
+  },
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61nV3oTC8vL._SX326_BO1,204,203,200_.jpg",
+    title: "Legend",
+    author: "Marie Lu",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61CzW79X7CL._SX331_BO1,204,203,200_.jpg",
+    title: "Prodigy",
+    author: "Marie Lu",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { books } from "./books";
+import Book from "./Book";
 import "./index.css";
-
-const books = [
-  {
-    id: 0,
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/41vw+RmEi4L._SX327_BO1,204,203,200_.jpg",
-    title: "The Martian",
-    author: "Andy Weir",
-  },
-  {
-    id: 1,
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/61nV3oTC8vL._SX326_BO1,204,203,200_.jpg",
-    title: "Legend",
-    author: "Marie Lu",
-  },
-  {
-    id: 2,
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/61CzW79X7CL._SX331_BO1,204,203,200_.jpg",
-    title: "Prodigy",
-    author: "Marie Lu",
-  },
-];
 
 function BookList() {
   return (
@@ -35,35 +13,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  // attribute, eventHandler
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert("hello world");
-  };
-  const complexExample = (author) => {
-    console.log(author);
-  };
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt="" />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        Reference example
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        More complex example
-      </button>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
